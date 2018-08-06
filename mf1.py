c944ec408e2a1930527346bd327c4d9cc7c3eda0
330mlcc <mlcc330@hotmail.com>
2018-08-06 22:29:11 +0800

    Initial commit

---
+++ mf1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+@version: v1.0 
+@author: 330mlcc 
+@license: Apache Licence  
+@contact: lg_online@126.com 
+@site:  
+@software: PyCharm 
+@file: mf1.py
+@time: 18-8-6 下午10:26 
+Description: 
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
